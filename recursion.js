940ff074165471cf4fbf2ed7c329c04303874fce
Oscar Robertson <86749444+itdepends-music@users.noreply.github.com>
2023-11-12 12:35:23 +1300

    Add recursion.js and other files

---
+++ recursion.js
@@ -0,0 +1,20 @@
+const fibs = (n) => {
+    if (n === 1) return [0];
+
+    const fibs = [0, 1];
+
+    for (let i = 1; i < n - 1; i++) {
+        fibs.push(fibs[fibs.length - 1] + fibs[fibs.length - 2]);
+    }
+
+    return fibs;
+};
+
+const fibsRec = (n) => {
+    if (n === 1) return [0];
+    if (n === 2) return [0, 1];
+    const prev = fibsRec(n - 1);
+    return prev.concat(prev[prev.length - 1] + prev[prev.length - 2]);
+};
+
+console.log(fibsRec(8));
